7d7d83e90ba291c8e2406b203b2ea8b02f6a4851
Josh Fletcher <jfletcher@nuxeo.com>
2018-01-09 17:49:43 -0800

    Use constants for param names.

---
+++ nuxeo-labs-geonames-core/src/main/java/org/nuxeo/labs/geonames/core/GeoNamesConstants.java
@@ -0,0 +1,14 @@
+package org.nuxeo.labs.geonames.core;
+
+public class GeoNamesConstants {
+  public static final String PARAM_POSTALCODE = "postalcode";
+  public static final String PARAM_COUNTRY = "country";
+  public static final String PARAM_RADIUS = "radius";
+  public static final String PARAM_MAXROWS = "maxRows";
+  public static final String PARAM_LAT = "lat";
+  public static final String PARAM_LNG = "lng";
+  public static final String PARAM_STYLE = "style";
+  public static final String PARAM_LOCALCOUNTRY = "localCountry";
+
+
+}
--- nuxeo-labs-geonames-core/src/main/java/org/nuxeo/labs/geonames/core/operations/FindNearbyPostalCodesOp.java
+++ nuxeo-labs-geonames-core/src/main/java/org/nuxeo/labs/geonames/core/operations/FindNearbyPostalCodesOp.java
@@ -9,11 +9,14 @@ import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.labs.geonames.core.GeoNamesConstants;
+
+import java.util.HashMap;
 
 /**
  *
  */
-@Operation(id = FindNearbyPostalCodesOp.ID, category = Constants.CAT_SERVICES, label = "Find Nearby Postal Codes", description = "Use GeoNames API to find nearby postal codes.")
+@Operation(id = FindNearbyPostalCodesOp.ID, category = Constants.CAT_SERVICES, label = "Find Nearby Postal Codes", description = "Use GeoNames API to find nearby postal codes. Pass either postalcode or lat and lng.")
 public class FindNearbyPostalCodesOp {
 
   public static final String ID = "GeoNames.FindNearbyPostalCodesOp";
@@ -21,15 +24,54 @@ public class FindNearbyPostalCodesOp {
   @Context
   protected CoreSession session;
 
-  @Param(name = "path", required = false)
-  protected String path;
+  @Param(name = GeoNamesConstants.PARAM_POSTALCODE, required = false)
+  protected String postalcode;
+
+  @Param(name = GeoNamesConstants.PARAM_COUNTRY, required = false)
+  protected String country;
+
+  @Param(name = GeoNamesConstants.PARAM_RADIUS, required = false)
+  protected String radius;
+
+  @Param(name = GeoNamesConstants.PARAM_MAXROWS, required = false)
+  protected String maxRows;
+
+  @Param(name = GeoNamesConstants.PARAM_LAT, required = false)
+  protected String lat;
+
+  @Param(name = GeoNamesConstants.PARAM_LNG, required = false)
+  protected String lng;
+
+  @Param(name = GeoNamesConstants.PARAM_STYLE, required = false)
+  protected String style;
+
+  @Param(name = GeoNamesConstants.PARAM_LOCALCOUNTRY, required = false)
+  protected String localCountry;
 
   @OperationMethod
   public DocumentModel run() {
-    if (StringUtils.isBlank(path)) {
+    HashMap<String, String> apiParams = new HashMap<String, String>();
+
+
+    if (!StringUtils.isBlank(postalcode)) {
+      // Use postalcode search
+      apiParams.put(GeoNamesConstants.PARAM_POSTALCODE, postalcode);
+      apiParams.put(GeoNamesConstants.PARAM_COUNTRY, country);
+      apiParams.put(GeoNamesConstants.PARAM_RADIUS, radius);
+      apiParams.put(GeoNamesConstants.PARAM_MAXROWS, maxRows);
+
       return session.getRootDocument();
     } else {
-      return session.getDocument(new PathRef(path));
+      // Use lat + lng search
+      apiParams.put(GeoNamesConstants.PARAM_LAT, lat);
+      apiParams.put(GeoNamesConstants.PARAM_LNG, lng);
+      apiParams.put(GeoNamesConstants.PARAM_RADIUS, radius);
+      apiParams.put(GeoNamesConstants.PARAM_MAXROWS, maxRows);
+      apiParams.put(GeoNamesConstants.PARAM_STYLE, style);
+      apiParams.put(GeoNamesConstants.PARAM_COUNTRY, country);
+      apiParams.put(GeoNamesConstants.PARAM_LOCALCOUNTRY, localCountry);
+
+      return session.getDocument(new PathRef(postalcode));
     }
   }
 }
